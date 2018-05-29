72a35fec2fea983efb99d883fccb7457799dd964
Rajiv Mittal <rajmittal09@gmail.com>
2018-05-29 14:01:33 -0400

    add

--- mbin/RTM_Example_2layer.m
+++ mbin/RTM_Example_2layer.m
@@ -1,15 +1,15 @@
 % standard FWI example
 clear all;clc
-pwd = curdir;
-addpath(genpath(pwd));
+curdir = pwd;
+addpath(genpath(curdir));
 
 % random seed
 s = RandStream('mt19937ar','Seed',1);
 RandStream.setGlobalStream(s);
 
 % define model
-z             = 0:5:1000;
-x             = 0:5:1000;
+z             = 0:5:500;
+x             = 0:5:500;
 v             = 1500*ones(length(z),length(x));
 v(50:end,:)   = 1800;
 v(80:end,:)   = 2200;
@@ -18,27 +18,27 @@ v(80:end,:)   = 2200;
 n             = size(v);
 model.n       = n;
 model.o       = [0 0];
-model.d       = [10 10];
+model.d       = [5 5];
 model.xt      = x;
 model.zt      = z;
 model.nb      = [60 60;60 60];
 
 % set up frequency
-model.freq    = [5:0.25:40];
+model.freq    = [5:0.5:40];
 model.nf      = numel(model.freq);
 model.f0      = 20; %peak freq of ricker wavelet
 model.t0      = 0; %phase shift of wavelet in seconds
 
 %receivers and sources near the top
 model.zsrc   = model.d(2);
-model.xsrc   = model.xt(1:5:end);
+model.xsrc   = model.xt(1:10:end);
 model.zrec   = model.d(2);
-model.xrec   = model.xt(1:2:end);
+model.xrec   = model.xt(1:end);
 ns           = length(model.xsrc);
 nr           = length(model.xrec);
 Q            = speye(ns);
 model.unit   = 's2/km2';
-m            = 1e6./v(:)^2;
+m            = 1e6./v(:).^2;
 % smooth model
 S           = opKron(opSmooth(n(2),10),opSmooth(n(1),10));
 m0          = S*m;
@@ -52,20 +52,21 @@ D0          = F(m0,Q,model);
 % linearized Data
 b           = D - D0;
 
-% curvelet operator
-C           = opWavelet(n(1),n(2),'HAAR',8,5);
+% wavelet operator
+C           = opWavelet2(n(1),n(2),'haar',8,5);
 
 % jacobian (operator A)
 J           = oppDF(m0,Q,model);
 A           = J*C';
 
 % plain RTM
-dm          = J'*b;
+tic;dm          = J'*b;toc
 dm          = reshape(dm,n);
 figure(1);imagesc(x,z,diff(dm,1));title('RTM');
 
 % Sparsity promition based LSRTM
-opts         = spgSetParms('iterations',20);
+opts         = spgSetParms('iterations',10,...
+                           'verbosity',2);
 [x,r,g,info] = spgl1(A,b,0,1e-3,[],opts);
 
 % LSRTM image
